07c85a91b7f87c12d08dc180a66847e6c6ba161b
Isaac Kalumba <ikalumba221@gmail.com>
2024-07-09 22:55:25 +0300

    add fifth passing test

--- test/test.js
+++ test/test.js
@@ -14,6 +14,9 @@ describe('Fibonacci Sequence', function() {
     });
     it('Should return correct sequence for n = 10', function() {
         assert.deepEqual(fibonacci(10), [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]);
-      });  
+      });
+    it('Should return correct sequence for n = 15', function() {
+        assert.deepEqual(fibonacci(15), [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377]);
+      });    
 
 });
